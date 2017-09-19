3847be94d64ac4e741a978d1f209d36ca3510dc2
ZhangMeng666666 <zm_happiness66@126.com>
2017-09-19 09:57:24 +0800

    20170919new

--- Data/sw.sql
+++ Data/sw.sql
@@ -1,5 +1,5 @@
 ﻿# Host: 127.0.0.1  (Version: 5.5.5-10.1.8-MariaDB)
-# Date: 2017-09-18 11:41:55
+# Date: 2017-09-19 09:57:05
 # Generator: MySQL-Front 5.3  (Build 4.271)
 
 /*!40101 SET NAMES utf8 */;
@@ -32,14 +32,16 @@ CREATE TABLE `channel` (
   `channel_title` varchar(255) DEFAULT NULL COMMENT '标题',
   `channel_sort` int(11) DEFAULT NULL COMMENT '排序（越大越靠前）',
   `channel_show` int(11) DEFAULT NULL COMMENT '是否显示（0否；1是）',
+  `channel_link` varchar(255) DEFAULT NULL COMMENT '直播源链接',
+  `channel_link_m` varchar(255) DEFAULT NULL COMMENT '手机版视频源链接',
   PRIMARY KEY (`channel_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='频道表';
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='频道表';
 
 #
 # Data for table "channel"
 #
 
-INSERT INTO `channel` VALUES (1,'CCTV-1',1,1),(2,'CCTV-2',0,0);
+INSERT INTO `channel` VALUES (1,'CCTV-1',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv1','http://live.aikan.miguvideo.com/wd_r2/cctv/cctv1hd/1200/01.m3u8'),(2,'CCTV-2',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv2','http://r.gslb.lecloud.com/live/hls/201706223000000bc99/desc.m3u8'),(3,'CCTV-3',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv3','http://r.gslb.lecloud.com/live/hls/201706223000000bf99/desc.m3u8'),(4,'CCTV-4',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv4','http://r.gslb.lecloud.com/live/hls/201706223000000bk99/desc.m3u8'),(5,'CCTV-5',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv5','http://r.gslb.lecloud.com/live/hls/201706223000000bp99/desc.m3u8'),(6,'CCTV-6',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv6','http://r.gslb.lecloud.com/live/hls/201706223000000br99/desc.m3u8'),(7,'CCTV-7',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv7','http://r.gslb.lecloud.com/live/hls/201706223000000bu99/desc.m3u8'),(8,'CCTV-8',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv8','http://r.gslb.lecloud.com/live/hls/201706223000000bv99/desc.m3u8'),(9,'CCTV-9',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv9','http://r.gslb.lecloud.com/live/hls/201706223000000bz99/desc.m3u8'),(10,'CCTV-10',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv10','http://r.gslb.lecloud.com/live/hls/201706223000000c199/desc.m3u8'),(12,'CCTV-11',0,1,'http://www.haotv8.com/hnws/fyzb.php?name=cctv11','http://r.gslb.lecloud.com/live/hls/201706223000000c499/desc.m3u8');
 
 #
 # Structure for table "program"
@@ -54,7 +56,7 @@ CREATE TABLE `program` (
   `program_sort` int(11) DEFAULT NULL COMMENT '排序（越大越靠前）',
   `program_show` int(11) DEFAULT NULL COMMENT '是否显示（0否；1是）',
   PRIMARY KEY (`program_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='节目表';
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='节目表';
 
 #
 # Data for table "program"
